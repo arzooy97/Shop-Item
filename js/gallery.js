564a72db78940838a1f0b9008c8ff36ada5c09c0
yadavarzoo <arzooy.co@nsit.net.in>
2018-06-27 19:07:55 +0530

    Readme updated and more features added for nice display of gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,17 @@
 (function($) {
-  $(document).ready(function() {
-    $(".img-responsive").fancybox();
-  });
-})(jQuery)
-
+  Drupal.behaviors.gallery = {
+    attach: function (context) {
+      $(".fancybox").fancybox({
+      	toolbar: true,
+      	buttons: [
+        "zoom",
+        "slideShow",
+        "fullScreen",
+        "download",
+        "thumbs",
+        "close"
+        ],
+      });
+    }
+  }
+})(jQuery);
